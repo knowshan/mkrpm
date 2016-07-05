adfbbaf6add967190a7ea41a0d0ca1862cf54709
Shantanu Pavgi <spavgi@guidewire.com>
2016-07-05 15:40:15 -0700

    Added option to pass config file

--- mkrpm.sh
+++ mkrpm.sh
@@ -19,7 +19,7 @@ exit 0
 
 function read_options(){
   # Get output (options list) from getopt  
-  TEMP_OPTS=`getopt -o hn:v:r: --longoptions help,name:,version:,release: -- $@`
+  TEMP_OPTS=`getopt -o hc:n:v:r: --longoptions help,configfile:,name:,version:,release: -- $@`
 
   # $? exit code will be non-zero if options passed using $@ aren't passed according getopt command options  
   if [ $? != 0 ]; then
@@ -37,7 +37,13 @@ function read_options(){
     case "$1" in
       -h|--help)
         echo 'help'
+        shift
         usage
+      ;;
+      # Config file listing package_name, package_version, package_release
+      -c|--config)
+        configfile="$2"
+        shift 2 
       ;;
       # Package name
       -n|--name)
@@ -113,6 +119,7 @@ fi
 
 read_options "$@"
 build_tools_check
+. $config_file
 
 # Set package name
 if [[ -z "$package_name" ]]; then
